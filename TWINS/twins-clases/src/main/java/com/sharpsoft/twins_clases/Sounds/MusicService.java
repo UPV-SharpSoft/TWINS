c696a082b19c9968f5dda4030df57afd77dec3de
aucejito <tomanotademicorreo@hotmail.com>
2020-04-08 20:19:17 +0200

    Clase MusicService
    No me funcionan ni los imports

---
+++ TWINS/twins-clases/src/main/java/com/sharpsoft/twins_clases/Sounds/MusicService.java
@@ -0,0 +1,12 @@
+package com.sharpsoft.twins_clases.Sounds;
+import java.net.URL;
+import javax.xml.namespace.QName;
+import javax.xml.ws.Service;
+
+
+public class MusicService extends Service implements MediaPlayer.OnErrorListener {
+
+    protected MusicService(URL wsdlDocumentLocation, QName serviceName) {
+        super(wsdlDocumentLocation, serviceName);
+    }
+}
