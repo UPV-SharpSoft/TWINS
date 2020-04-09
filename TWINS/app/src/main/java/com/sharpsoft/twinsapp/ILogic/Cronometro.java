aa37579850eca481013055a6cbcb26839696eda1
Marcos <markettes_99@outlook.es>
2020-04-09 18:28:06 +0200

    Corrección Cronómetro
    Preparado para pegar en la posición correspondiente

--- TWINS/app/src/main/java/com/sharpsoft/twinsapp/ILogic/Cronometro.java
+++ TWINS/app/src/main/java/com/sharpsoft/twinsapp/ILogic/Cronometro.java
@@ -4,27 +4,41 @@ import android.os.CountDownTimer;
 import android.widget.TextView;
 
 import java.text.CollationElementIterator;
+import java.util.Formatter;
 
 public class Cronometro {
 
     private TextView textViewChrono;
     private TextView textViewFinish;
+    private CountDownTimer workingCountDown;
+    private Formatter millisCountFormat;
 
-    public Cronometro(){}
-
-    public void countDown() {
-
-        CountDownTimer workingCountDown = new CountDownTimer(15000, 1000) {
+    public Cronometro(int segundos){
+        workingCountDown = new CountDownTimer(segundos*1000, 100) {
             @Override
             public void onTick(long millisUntilFinished) {
-                textViewChrono.setText("" + millisUntilFinished / 1000);
+                if(millisUntilFinished>10000) {
+                    textViewChrono.setText("" + millisUntilFinished / 1000);
+                } else {
+                    textViewChrono.setText("" + millisUntilFinished / 1000.0);
+                }
             }
 
             @Override
             public void onFinish() {
+                /* ToDo */
                 textViewFinish.setText("TIME OUT!");
             }
         };
+    }
 
+    public void start() {
+        //Hacer en hilo UI
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                workingCountDown.start();
+            }
+        }).start();
     }
 }
