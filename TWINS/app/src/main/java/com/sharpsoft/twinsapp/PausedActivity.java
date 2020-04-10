f3f9ea5803e713c82fce3c3b16018bf96a3033ff
diegoroig <diego.roigdrh@gmail.com>
2020-04-10 12:01:38 +0200

    Funcionalidad Close Paused

--- TWINS/app/src/main/java/com/sharpsoft/twinsapp/PausedActivity.java
+++ TWINS/app/src/main/java/com/sharpsoft/twinsapp/PausedActivity.java
@@ -3,12 +3,25 @@ package com.sharpsoft.twinsapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class PausedActivity extends AppCompatActivity {
+
+    private ImageButton imageButtonClose;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_paused);
+
+
+        ImageButton imageButtonClose = findViewById(R.id.imageButtonClose);
+        imageButtonClose.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
     }
 }
