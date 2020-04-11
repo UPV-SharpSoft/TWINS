a80a1607b761e59e56e45833ef1008cd1157d203
Niko <NikoConn@users.noreply.github.com>
2020-04-11 16:10:14 +0200

    Mover codigo e intentar arreglar las cartas

--- TWINS/app/src/main/java/com/sharpsoft/twinsapp/Juego.java
+++ TWINS/app/src/main/java/com/sharpsoft/twinsapp/Juego.java
@@ -7,7 +7,10 @@ import android.content.Context;
 import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.os.CountDownTimer;
+import android.util.Log;
+import android.view.Gravity;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ImageButton;
 import android.widget.LinearLayout;
 import android.widget.TextView;
@@ -39,24 +42,16 @@ public class Juego extends AppCompatActivity {
         setContentView(R.layout.activity_juego);
 
         cronoTV = findViewById(R.id.cronoTV);
+        tableroLayout = findViewById(R.id.tableroLayout);
+        imageButtonPause = findViewById(R.id.imageButtonPause);
+
         instanciarCronometro();
-        tableroLayout = findViewById(R.id.tableroLinearLayout);
 
         Baraja baraja = BarajaFactory.getBaraja(BarajaFactory.Barajas.minecraft, 20, this);
         tablero = new ITablero(4,5, baraja, this);
 
-        for(int y = 0; y < tablero.getHeight(); y++){
-            LinearLayout horizontalLayout = new LinearLayout(this);
-            horizontalLayout.setOrientation(LinearLayout.HORIZONTAL);
-            for(int x = 0; x < tablero.getWidth(); x++){
-                ICarta carta = (ICarta) tablero.getCarta(x, y);
-                View cartaView = carta.getCartaView();
-                horizontalLayout.addView(cartaView);
-            }
-            tableroLayout.addView(horizontalLayout);
-        }
-
-        imageButtonPause = findViewById(R.id.imageButtonPause);
+        View tableroView = ((ITablero) tablero).getView(this);
+        tableroLayout.addView(tableroView);
 
         imageButtonPause.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -83,7 +78,7 @@ public class Juego extends AppCompatActivity {
 
     }
 
-    public void instanciarCronometro(){
+    private void instanciarCronometro(){
         cronometro = new Cronometro(60, cronoTV, this);
     }
 
