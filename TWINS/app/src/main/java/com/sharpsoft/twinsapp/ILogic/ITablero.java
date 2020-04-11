a6815ac609852bfb324a7bb3e1f00ee2639e9e4b
Niko <NikoConn@users.noreply.github.com>
2020-04-11 19:52:21 +0200

    margenes entre las cartas

--- TWINS/app/src/main/java/com/sharpsoft/twinsapp/ILogic/ITablero.java
+++ TWINS/app/src/main/java/com/sharpsoft/twinsapp/ILogic/ITablero.java
@@ -3,6 +3,7 @@ package com.sharpsoft.twinsapp.ILogic;
 import android.content.Context;
 import android.view.Gravity;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.LinearLayout;
 
 import com.sharpsoft.twins_clases.logic.Tablero;
@@ -27,6 +28,7 @@ public class ITablero extends Tablero {
         tableroLayout.setOrientation(LinearLayout.VERTICAL);
         tableroLayout.setGravity(Gravity.CENTER);
 
+
         for(int y = 0; y < this.getHeight(); y++){
             LinearLayout horizontalLayout = new LinearLayout(ctx);
             horizontalLayout.setOrientation(LinearLayout.HORIZONTAL);
@@ -34,6 +36,12 @@ public class ITablero extends Tablero {
             for(int x = 0; x < this.getWidth(); x++){
                 ICarta carta = (ICarta) this.getCarta(x, y);
                 View cartaView = carta.getCartaView(ctx, horizontalLayout);
+
+                ViewGroup.MarginLayoutParams marginParams = new ViewGroup.MarginLayoutParams(cartaView.getLayoutParams());
+                marginParams.setMargins(5,5,5,5);
+                LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(marginParams);
+                cartaView.setLayoutParams(layoutParams);
+
                 horizontalLayout.addView(cartaView);
             }
             tableroLayout.addView(horizontalLayout);
