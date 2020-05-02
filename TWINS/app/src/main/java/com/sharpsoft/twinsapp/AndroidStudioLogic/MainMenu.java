f4fc67f3b91e33bcd0eb4acfc81c2a3a83ff90ba
Niko <NikoConn@users.noreply.github.com>
2020-05-03 00:13:05 +0200

    Main menu terminado
    falta enlazar con otras activities y los iconos de abajo

--- TWINS/app/src/main/java/com/sharpsoft/twinsapp/AndroidStudioLogic/MainMenu.java
+++ TWINS/app/src/main/java/com/sharpsoft/twinsapp/AndroidStudioLogic/MainMenu.java
@@ -9,9 +9,11 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import com.sharpsoft.twinsapp.Game;
-import com.sharpsoft.twinsapp.MainActivity;
 import com.sharpsoft.twinsapp.R;
 
 public class MainMenu extends AppCompatActivity {
@@ -26,6 +28,7 @@ public class MainMenu extends AppCompatActivity {
         setContentView(R.layout.activity_main_menu);
 
         ImageButton closeAppButton = findViewById(R.id.closeAppButton);
+        Button newGameButton = findViewById(R.id.newGameButton);
 
         closeAppButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -34,19 +37,37 @@ public class MainMenu extends AppCompatActivity {
             }
         });
 
-        Button newGameButton = findViewById(R.id.button);
         newGameButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent i = new Intent(MainMenu.this, Game.class);
-                startActivity(i);
-                finish();
+                setNewGameUI();
             }
         });
     }
 
     private void setNewGameUI(){
+        setContentView(R.layout.new_game);
 
+        ImageButton homeImageButton = findViewById(R.id.homeImageButton);
+        ImageView partidaRapidaImageView = findViewById(R.id.partidaRapidaImageView);
+        TextView partidaRapidaTextView = findViewById(R.id.partidaRapidaTextView);
+
+        View.OnClickListener partidaRapidaClick = new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(MainMenu.this, Game.class);
+                startActivity(i);
+            }
+        };
+        homeImageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                setMainUI();
+            }
+        });
+
+        partidaRapidaImageView.setOnClickListener(partidaRapidaClick);
+        partidaRapidaTextView.setOnClickListener(partidaRapidaClick);
     }
 
     private void showExitConfirmation(){
