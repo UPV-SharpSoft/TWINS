e5023fb8d97eed705f814f506903512d3cd4e9fc
aucejito <tomanotademicorreo@hotmail.com>
2020-05-05 12:36:45 +0200

    Segunda baraja

--- TWINS/app/src/main/java/com/sharpsoft/twinsapp/AndroidStudioLogic/DeckFactory.java
+++ TWINS/app/src/main/java/com/sharpsoft/twinsapp/AndroidStudioLogic/DeckFactory.java
@@ -16,11 +16,13 @@ public class DeckFactory {
 
     public enum Decks {
         minecraft,
-        ESPAÑOLA
+        ESPAÑOLA,
+        fruits
     }
 
     private final static String[] minecraftCards = {"RedstoneOre.png", "DiamondOre.png", "GoldOre.png",
                                         "LapislazuliOre.png", "EmeraldOre.png", "IronOre.png"};
+    private final static String[] fruitCards = {"apple.png", "banana.png", "carrot.png", "eggplant.png", "kiwi.png", "lemon.png", "orange.png", "pear.png", "pineapple.png", "tomato.png", "watermelon.png"};
 
     public static Deck getDeck(Decks deck, Dimension dimension, Context ctx){
         List<Bitmap> cards = new ArrayList<>();
@@ -34,6 +36,13 @@ public class DeckFactory {
             }
             reverse = getBitmapFromAsset("CartasMinecraft/back.png", ctx);
             name = "Minecraft";
+        }else if(deck == Decks.fruits){
+            for(int i = 0; i < dimension.getTotal()/2; i++){
+                Bitmap b = getBitmapFromAsset("FruitsDeck/" + fruitCards[i% fruitCards.length], ctx);
+                cards.add(b); cards.add(b);
+            }
+            reverse = getBitmapFromAsset("FruitsDeck/backFruit.png", ctx);
+            name = "Fruits";
         }
 
         return new Deck(cards, reverse, name);
@@ -44,6 +53,10 @@ public class DeckFactory {
         if(deck == Decks.minecraft){
             for(String s : minecraftCards){
                 res.add(getBitmapFromAsset("CartasMinecraft/" + s, ctx));
+            }
+        }else if(deck == Decks.fruits){
+            for(String s : minecraftCards){
+                res.add(getBitmapFromAsset("FruitsDeck/" + s, ctx));
             }
         }
         return res;
