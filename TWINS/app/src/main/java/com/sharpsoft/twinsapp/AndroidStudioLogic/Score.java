024af0c6cac9e1f929805eadbd99f4344537cca5
Niko <NikoConn@users.noreply.github.com>
2020-05-05 12:55:58 +0200

    Animacion de cambio de puntuacion

--- TWINS/app/src/main/java/com/sharpsoft/twinsapp/AndroidStudioLogic/Score.java
+++ TWINS/app/src/main/java/com/sharpsoft/twinsapp/AndroidStudioLogic/Score.java
@@ -12,32 +12,87 @@ public class Score extends com.sharpsoft.twins_clases.logic.Score {
         this.scoreTextView = scoreTextView;
     }
 
+    private void setScoreTextView(final int score, int leftScore){
+        final String StringScore = String.valueOf(score);
+        String StringLeftScore = "";
+        if(leftScore > 0){
+            StringLeftScore = "+" + leftScore;
+            scoreTextView.setTextColor(Color.BLACK);
+        }else if (leftScore < 0){
+            StringLeftScore = String.valueOf(leftScore);
+            scoreTextView.setTextColor(Color.RED);
+        }else{
+            if (score < 0) {
+                scoreTextView.setTextColor(Color.RED);
+            }else{
+                scoreTextView.setTextColor(Color.BLACK);
+            }
+        }
+
+        final String finalStringLeftScore = StringLeftScore;
+        ((Activity)scoreTextView.getContext()).runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                scoreTextView.setText(StringScore + "  " + finalStringLeftScore);
+            }
+        });
+    }
+
+    private void auxSleep(int time){
+        try {
+            Thread.sleep(time);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void animAux(final boolean firstTime){
+        Thread t = new Thread(){
+            public void run(){
+                int textViewCurrentScore = Integer.parseInt(String.valueOf(scoreTextView.getText()).split(" ")[0]);
+                int score = getScore();
+                if(textViewCurrentScore != score){
+                    int diff = score - textViewCurrentScore;
+
+                    if(firstTime){
+                        setScoreTextView(textViewCurrentScore, diff);
+                        auxSleep(500);
+                    }
+
+                    if(diff < 0){
+                        diff++;
+                        textViewCurrentScore--;
+                    }else{
+                        diff--;
+                        textViewCurrentScore++;
+                    }
+
+                    setScoreTextView(textViewCurrentScore, diff);
+
+                    auxSleep(50);
+                    animAux(false);
+                }
+            }
+        };
+        t.start();
+    }
+
+    private void anim(){
+        animAux(true);
+    }
+
     public void correct(){
         super.correct();
-        scoreTextView.setText(String.valueOf(getScore()));
-        if (getScore() > 0) {
-            scoreTextView.setTextColor(Color.BLACK);
-        }
+        anim();
     }
 
     public void fail(){
         super.fail();
-        scoreTextView.setText(String.valueOf(getScore()));
-        if (getScore() < 0) {
-            scoreTextView.setTextColor(Color.RED);
-        }
-        }
+        anim();
+    }
 
     public void missedTurn(){
         super.missedTurn();
-        ((Activity)scoreTextView.getContext()).runOnUiThread(new Runnable() {
-            @Override
-            public void run() {
-                scoreTextView.setText(String.valueOf(getScore()));
-                if (getScore() < 0) {
-                    scoreTextView.setTextColor(Color.RED);
-                }
-            }
-        });
+        anim();
     }
 }
