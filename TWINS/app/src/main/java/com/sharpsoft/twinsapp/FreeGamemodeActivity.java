69bec9539c2c924fc52e49aa0435cdefe401808b
aucejito <tomanotademicorreo@hotmail.com>
2020-05-11 23:44:20 +0200

    Ni idea

--- TWINS/app/src/main/java/com/sharpsoft/twinsapp/FreeGamemodeActivity.java
+++ TWINS/app/src/main/java/com/sharpsoft/twinsapp/FreeGamemodeActivity.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
 
@@ -11,7 +12,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 
 
-public class FreeGamemodeActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
+public class FreeGamemodeActivity extends AppCompatActivity{
 
     private Spinner deckSpinner;
     private Spinner boardSpinner;
@@ -21,6 +22,14 @@ public class FreeGamemodeActivity extends AppCompatActivity implements AdapterVi
     private EditText totalTime;
     private EditText turnTime;
     private EditText showCardTime;
+
+    private Button playButton;
+
+    private ArrayAdapter<CharSequence> adapterAudioPack;
+    private ArrayAdapter<CharSequence> adapterGamemodes;
+    private ArrayAdapter<CharSequence> adapterBoard;
+    private ArrayAdapter<CharSequence> adapterBoard2;
+    private ArrayAdapter<CharSequence> adapterDeck;
 
 
 
@@ -36,44 +45,76 @@ public class FreeGamemodeActivity extends AppCompatActivity implements AdapterVi
         gamemodeSpinner = findViewById(R.id.gamemodeSpinner);
         totalTime = findViewById(R.id.totalTime);
         turnTime = findViewById(R.id.turnTime);
-        showCardTime = findViewById(R.id.turnTime);
+        showCardTime = findViewById(R.id.showCardTime);
+
+        playButton = findViewById(R.id.playButton);
 
         setValues();
+
+        boardSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener(){
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                if(boardSpinner.getSelectedItem().toString().equals("3") ||
+                        boardSpinner.getSelectedItem().toString().equals("5")){
+                    boardSpinner2.setAdapter(adapterBoard2);
+                }
+
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {}
+        });
+        boardSpinner2.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener(){
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                if(boardSpinner2.getSelectedItem().toString().equals("3") ||
+                        boardSpinner2.getSelectedItem().toString().equals("5") ){boardSpinner.setAdapter(adapterBoard2);}
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {}
+        });
+
+        playButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                if(showCardTime == null){
+                    deckSpinner.animate().rotation(5).rotation(-10).rotation(5).start();
+                }else
+                setGame();
+            }
+        });
+
     }
 
     public void setValues(){
-        ArrayAdapter<CharSequence> adapterGamemodes = ArrayAdapter.createFromResource(this, R.array.gamemode, android.R.layout.simple_spinner_item);
+        adapterGamemodes = ArrayAdapter.createFromResource(this, R.array.gamemode, android.R.layout.simple_spinner_item);
         adapterGamemodes.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         gamemodeSpinner.setAdapter(adapterGamemodes);
 
-
-        ArrayAdapter<CharSequence> adapterAudioPack = ArrayAdapter.createFromResource(this, R.array.audioPack, android.R.layout.simple_spinner_item);
+        adapterAudioPack = ArrayAdapter.createFromResource(this, R.array.audioPack, android.R.layout.simple_spinner_item);
         adapterAudioPack.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         soundSpinner.setAdapter(adapterAudioPack);
 
-        ArrayAdapter<CharSequence> adapterBoard = ArrayAdapter.createFromResource(this, R.array.boardDimension, android.R.layout.simple_spinner_item);
+        adapterBoard = ArrayAdapter.createFromResource(this, R.array.boardDimension, android.R.layout.simple_spinner_item);
         adapterBoard.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        adapterBoard2 = ArrayAdapter.createFromResource(this, R.array.boardDimension2, android.R.layout.simple_spinner_item);
+        adapterBoard2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         boardSpinner.setAdapter(adapterBoard);
         boardSpinner2.setAdapter(adapterBoard);
 
-        turnTime.setText("Tiempo de turno para girar la segunda carta. Entre 3-10 segundos");
-        showCardTime.setText("Tiempo que se mostrarán las cartas al inicio. Entre 0-10 segundos");
-        totalTime.setText("Tiempo total de la partida. Entre 20 - 120 segundos");
-
-        ArrayAdapter<CharSequence> adapterDeck = ArrayAdapter.createFromResource(this, R.array.deck, android.R.layout.simple_spinner_item);
+        adapterDeck = ArrayAdapter.createFromResource(this, R.array.deck, android.R.layout.simple_spinner_item);
         adapterDeck.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         deckSpinner.setAdapter(adapterDeck);
 
+        turnTime.setHint("3-10 segundos");
+        showCardTime.setHint("0-10 segundos");
+        totalTime.setHint("20 - 120 segundos");
     }
 
+    public void setGame(){
 
-    @Override
-    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
 
-    }
-
-    @Override
-    public void onNothingSelected(AdapterView<?> parent) {
 
     }
 }
