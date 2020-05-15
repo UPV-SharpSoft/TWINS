e99ddc6d0fdd694e2b5ef594cabe228ad7616547
aucejito <tomanotademicorreo@hotmail.com>
2020-05-15 19:46:17 +0200

    NO SÉ HACER INTERFACES, ES IMPOSIBLE

--- TWINS/app/src/main/java/com/sharpsoft/twinsapp/NewDeckActivity.java
+++ TWINS/app/src/main/java/com/sharpsoft/twinsapp/NewDeckActivity.java
@@ -11,6 +11,7 @@ import android.media.Image;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
+import android.text.Layout;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -19,7 +20,9 @@ import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.Space;
 import android.widget.Spinner;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.sharpsoft.twins_clases.logic.Dimension;
@@ -35,10 +38,9 @@ import java.util.regex.Pattern;
 
 public class NewDeckActivity extends AppCompatActivity {
 
-    private Button buttonLoadImage;
+    private ImageButton buttonLoadImage;
     private ImageView imageViewDeck;
     private static int RESULT_LOAD_IMAGE = 1;
-    private Spinner spinnerDeck;
     private ImageView previousCard;
     private ImageView nextCard;
     private Button buttonSave;
@@ -54,98 +56,14 @@ public class NewDeckActivity extends AppCompatActivity {
         overridePendingTransition(R.anim.fadein, R.anim.fadeout);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_deck);
-
         buttonLoadImage = findViewById(R.id.buttonLoadImage);
-        spinnerDeck = findViewById(R.id.spinnerDeck);
-        imageViewDeck = findViewById(R.id.imageViewDeck);
-        previousCard = findViewById(R.id.previousCard);
-        nextCard = findViewById(R.id.nextCard);
         buttonSave = findViewById(R.id.buttonSave);
         editTextName = findViewById(R.id.editTextName);
-
-        selectDeck();
-
-        moveCard();
-
-        addCard();
 
         uploadCard();
 
         createDeck();
 
-    }
-
-    private void selectDeck() {
-        ArrayList<String> arrayList = new ArrayList<>();
-        arrayList.addAll(getAllDecks());
-
-        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, arrayList);
-        arrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        spinnerDeck.setAdapter(arrayAdapter);
-
-        spinnerDeck.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-            @Override
-            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
-                selectedDeck = DeckFactory.getAllImages(DeckFactory.Decks.values()[i], NewDeckActivity.this);
-                imageViewDeck.setImageBitmap(selectedDeck.get(0));
-            }
-
-            @Override
-            public void onNothingSelected(AdapterView<?> adapterView) {
-
-            }
-        });
-    }
-
-    private void moveCard() {
-
-        previousCard.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                count = (count + selectedDeck.size() - 1) % selectedDeck.size();
-                imageViewDeck.setImageBitmap(selectedDeck.get(count));
-            }
-        });
-
-        nextCard.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                count = (count + 1) % selectedDeck.size();
-                imageViewDeck.setImageBitmap(selectedDeck.get(count));
-            }
-        });
-    }
-
-    private void addCard() {
-
-        imageViewDeck.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                final LinearLayout layout = findViewById(R.id.upperLinearLayout);
-
-                if (layout.getChildCount() >= 12) {
-                    Toast.makeText(NewDeckActivity.this, "Has alcanzado el máximo de cartas!", Toast.LENGTH_LONG).show();
-                    return;
-                }
-
-                ImageView cardValue = new ImageView(NewDeckActivity.this);
-                cardValue.setImageDrawable(imageViewDeck.getDrawable());
-                cardValue.setScaleType(ImageView.ScaleType.FIT_CENTER);
-
-                LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(250, 300);
-                cardValue.setLayoutParams(layoutParams);
-
-                cardValue.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View view) {
-                        layout.removeView(view);
-                    }
-                });
-
-                layout.addView(cardValue);
-
-            }
-        });
     }
 
     private void uploadCard() {
@@ -200,16 +118,6 @@ public class NewDeckActivity extends AppCompatActivity {
 
     }
 
-    private List<String> getAllDecks() {
-        List<String> res = new ArrayList<>();
-        Dimension d = new Dimension(2, 1);
-        for (DeckFactory.Decks decks : DeckFactory.Decks.values()) {
-            Deck deck = DeckFactory.getDeck(decks,d, 1,this);
-            res.add(deck.getName());
-        }
-        return res;
-    }
-
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
@@ -222,10 +130,21 @@ public class NewDeckActivity extends AppCompatActivity {
         }
 
         ImageView cardValue = new ImageView(NewDeckActivity.this);
-        cardValue.setImageDrawable(imageViewDeck.getDrawable());
+        cardValue.setImageDrawable(buttonLoadImage.getDrawable());
         cardValue.setScaleType(ImageView.ScaleType.FIT_CENTER);
 
-        LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(250, 300);
+
+        /*if(layout.getChildCount()==0){
+            TextView textReverse = new TextView(NewDeckActivity.this);
+            textReverse.setText("Reverso");
+            textReverse.setTextColor(111125);
+            layout.addView(textReverse, 800,800);
+
+
+        }*/
+
+
+        LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(500, 600);
         cardValue.setLayoutParams(layoutParams);
 
         layout.addView(cardValue);
