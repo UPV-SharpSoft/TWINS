6fc4e4bf33d9374347d4b97f6650bb49062a5417
Marcos <markettes_99@outlook.es>
2020-05-23 10:17:20 +0200

    constructor

--- TWINS/app/src/main/java/com/sharpsoft/twinsapp/AndroidStudioLogic/Player.java
+++ TWINS/app/src/main/java/com/sharpsoft/twinsapp/AndroidStudioLogic/Player.java
@@ -7,12 +7,21 @@ public class Player {
     private String nickname;
     private Turn turno;
 
+    public Player(int color, String nickname) {
+        this.color = color;
+        this.nickname = nickname;
+    }
+
     public String getNickname() {
         return nickname;
     }
 
     public int getColor() {
         return color;
+    }
+
+    public void setTurno(Turn turno) {
+        this.turno = turno;
     }
 
     public Turn getTurno() {
