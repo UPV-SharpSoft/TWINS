543522a4b9bf535cc62ec1a967baf36025887526
Marcos <markettes_99@outlook.es>
2020-05-23 10:15:38 +0200

    player

--- TWINS/app/src/main/java/com/sharpsoft/twinsapp/AndroidStudioLogic/Player.java
+++ TWINS/app/src/main/java/com/sharpsoft/twinsapp/AndroidStudioLogic/Player.java
@@ -1,8 +1,11 @@
 package com.sharpsoft.twinsapp.AndroidStudioLogic;
 
+import com.sharpsoft.twins_clases.logic.Turn;
+
 public class Player {
     private int color;
     private String nickname;
+    private Turn turno;
 
     public String getNickname() {
         return nickname;
@@ -10,5 +13,9 @@ public class Player {
 
     public int getColor() {
         return color;
+    }
+
+    public Turn getTurno() {
+        return turno;
     }
 }
